99bbafc57a5a8b8b478c8b3758d72bec55f6a688
stein <tairstenen@gmail.com>
2024-12-09 20:16:19 +0100

    added examples

--- day2/s.py
+++ day2/s.py
@@ -15,20 +15,24 @@ def first():
     print(sum)
 
 def second():
-  with open("day2/i") as f:
+  with open("day2/iex") as f:
     sum = 0
     for line in f:
       line = list(map(int, line.split()))
-      interval = [[0, 4], [-4, 0]]
-      errors1 = 0
-      errors2 = 0
+      interval = [0, 4] if line[1] - line[0] > 0 else [-4, 0]
+      errors = 0 
+      index = 1
       for i in range(1, len(line)):
-        diff = line[i] - line[i-1]
-        if diff <= interval[0][0] or diff >= interval[0][1]:
-          errors1 += 1
-        if diff <= interval[1][0] or diff >= interval[1][1]:
-          errors2 += 1
-      if errors1 <= 1 or errors2 <= 1:
+        diff = line[index] - line[index-1]
+        if diff <= interval[0] or diff >= interval[1]:
+          line.pop(index-1)
+          index -= 1
+          errors += 1
+          if errors > 1:
+            break
+        index += 1
+      if errors <= 1:
+        print(line)
         sum += 1
     print(sum)
 
